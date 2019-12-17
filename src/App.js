b6df1a30a802bbb2526d0faf4250f6f16070c015
tejasmouli <44651691+tejasmouli@users.noreply.github.com>
2019-12-17 21:32:27 +0530

    Removed Done component and made the app responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Todo from './components/Todo'
 
 function App() {
-
+  const [addItem, setAddItem] = useState(false);
   const todo = [
     {done: true, note: "Hi Hello", editState: false},
     {done: false, note: "No i cant do it now", editState: false},
-    {done: false, note: "The World is so big that you will need a lot of money to complete one round around it. The World is so big that you will need a lot of money to complete one round around it.", editState: false}
+    {done: false, note: "The World is so big that you will need a lot of money to complete one round around it. The World is so big that you will need a lot of money to complete one round around it.", editState: false},
+    {done: false, note: "No i cant do it now", editState: false},
   ]
 
   return (
     <div className="container">
       <div className="bd row">
-        <h2 className="col-10">Todo</h2>
+        <div className="col-10">
+          <h2>Todo</h2>
+        </div>
         <div className="col-2">
           <button><i className="material-icons">add</i></button>
         </div>
       </div>
       {todo.map(t =>
-      <Todo {...t}/>)}
+      <Todo {...t}/>
+      )}
+      <input type="text"/>
     </div>
     );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import './Todo.css';
-import Done from './Done'
+import './Done.css';
 
 const Todo = props => {
-    const [todoItem, setTodoItem] = useState(props ? props : {done: false, note: "", editState: true})
+    const [todoItem, setTodoItem] = useState(props ? props : {done: false, note: " ", editState: true})
 
     let rowsOfText = todoItem.note.length/40<1?1:todoItem.note.length/40;
 
@@ -23,17 +23,23 @@ const Todo = props => {
 
     return (
         <div className="row">
-            <Done markDone={toggleDone} className="col-1" checked={!todoItem.done}/>
-            <textarea
-                rows={rowsOfText}
-                onKeyDown={keyPress}
-                onClick={toggleEdit}
-                onChange={onChangeHandler}
-                className={todoItem.done ? "col-10 strick":"col-10"}
-                value={todoItem.note}
-                readOnly={!todoItem.editState}
-            />
-            <i className="material-icons">info_outlined</i>
+            <div className="col-1 col-xs-1">
+                <div className={todoItem.done?"box":"box checked"} onClick={toggleDone}></div>
+            </div>
+            <div className="col-9 col-xs-9">
+                <textarea
+                    rows={rowsOfText}
+                    onKeyDown={keyPress}
+                    onClick={toggleEdit}
+                    onChange={onChangeHandler}
+                    className={todoItem.done ? "strick":""}
+                    value={todoItem.note} 
+                    readOnly={!todoItem.editState}
+                />
+            </div>
+            <div className="col-1 col-xs-1">
+                <i className="material-icons">info_outlined</i>
+            </div>
         </div>
     )
 };
