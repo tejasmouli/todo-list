75ee4c8da7b09e2ea62266681c66271e70dc6053
tejasmouli <44651691+tejasmouli@users.noreply.github.com>
2019-12-18 01:02:54 +0530

    UI with all the neccessary functions implemented, need to implement the logic for localStorage of the data

--- src/App.js
+++ src/App.js
@@ -3,13 +3,59 @@ import './App.css';
 import Todo from './components/Todo'
 
 function App() {
-  const [addItem, setAddItem] = useState(false);
+  // const [addItem, setAddItem] = useState(false);
   const todo = [
-    {done: true, note: "Hi Hello", editState: false},
-    {done: false, note: "No i cant do it now", editState: false},
-    {done: false, note: "The World is so big that you will need a lot of money to complete one round around it. The World is so big that you will need a lot of money to complete one round around it.", editState: false},
-    {done: false, note: "No i cant do it now", editState: false},
+    {key:1, done: true, note: "Hi Hello", editState: false},
+    {key:2, done: false, note: "No i cant do it now", editState: false},
+    {key:3, done: false, note: "The World is so big that you will need a lot of money to complete one round around it. The World is so big that you will need a lot of money to complete one round around it.", editState: false},
+    {key:4, done: false, note: "No i cant do it now", editState: false}
   ]
+
+  const [todos, setTodos] = useState(todo);
+
+  const deleteItem = key => {
+    setTodos(todos.filter(x => x.key !== key))
+  };
+
+  const toggleItemEdit = key => {
+    let newTodos = [];
+    for (let i = 0; i < todos.length; i++) {
+      let item = todos[i];
+      if(item.key === key)
+        item.editState = !item.editState;
+      newTodos.push(item);
+    }
+    setTodos(newTodos);
+  }
+
+  const toggleItemDone = key => {
+    let newTodos = [];
+    for (let i = 0; i < todos.length; i++) {
+      let item = todos[i];
+      if(item.key === key)
+        item.done = !item.done;
+      newTodos.push(item);
+    }
+    setTodos(newTodos);
+  }
+
+  const onItemNoteChangeHandler = (key, note) => {
+    let newTodos = [];
+    for (let i = 0; i < todos.length; i++) {
+      let item = todos[i];
+      if(item.key === key)
+        item.note = note;
+      newTodos.push(item);
+    }
+    setTodos(newTodos);
+  }
+
+  const addTodo = () => {
+    if(todos.length === 0)
+      setTodos([{key: 1,done: false, note: "", editState: false}]);
+    else
+      setTodos([...todos, {key: todos.pop().key+1,done: false, note: "", editState: false}]);
+  };
 
   return (
     <div className="container">
@@ -18,13 +64,19 @@ function App() {
           <h2>Todo</h2>
         </div>
         <div className="col-2">
-          <button><i className="material-icons">add</i></button>
+          <button onClick={addTodo}><i className="material-icons">add</i></button>
         </div>
       </div>
-      {todo.map(t =>
-      <Todo {...t}/>
+      {todos.map(t => 
+        <Todo
+          key={t.key}
+          item = {t}
+          toggleEdit={toggleItemEdit}
+          toggleDone={toggleItemDone}
+          onNoteChangeHandler={onItemNoteChangeHandler}
+          removeThis = {deleteItem}
+        />
       )}
-      <input type="text"/>
     </div>
     );
 }
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,13 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Todo.css';
-import './Done.css';
 
 const Todo = props => {
-    const [todoItem, setTodoItem] = useState(props ? props : {done: false, note: " ", editState: true})
 
-    let rowsOfText = todoItem.note.length/40<1?1:todoItem.note.length/40;
-
-    const toggleEdit = () => {setTodoItem({...todoItem, editState: !todoItem.editState})};
+    const toggleEdit = () => {props.toggleEdit(props.item.key)};
 
     const keyPress = event => {
         if (event.key === 'Enter')
@@ -15,30 +11,32 @@ const Todo = props => {
     }
 
     const onChangeHandler = event => {
-        setTodoItem({...todoItem, note: event.target.value});
-        rowsOfText = todoItem.note.length/40<1?1:todoItem.note.length/40;
+        props.onNoteChangeHandler(props.item.key, event.target.value);
     }
 
-    const toggleDone = () => setTodoItem({...todoItem,done: !todoItem.done}) ;
+    const deleteItem = () => props.removeThis(props.item.key);
+
+    const toggleDone = () => props.toggleDone(props.item.key);
 
     return (
         <div className="row">
             <div className="col-1 col-xs-1">
-                <div className={todoItem.done?"box":"box checked"} onClick={toggleDone}></div>
+                <div className={props.item.done?"box":"box checked"} onClick={toggleDone}></div>
             </div>
-            <div className="col-9 col-xs-9">
-                <textarea
-                    rows={rowsOfText}
+            <div className="col-9 bl col-xs-9">
+                {props.item.editState
+                ?<input
+                    autoFocus
+                    onBlur={toggleEdit}
                     onKeyDown={keyPress}
-                    onClick={toggleEdit}
                     onChange={onChangeHandler}
-                    className={todoItem.done ? "strick":""}
-                    value={todoItem.note} 
-                    readOnly={!todoItem.editState}
+                    className={props.item.done ? "strick":""}
+                    value={props.item.note}
                 />
+                :<div className={props.item.done ? "note strick":"note"} onClick={toggleEdit}>{props.item.note}</div>}
             </div>
             <div className="col-1 col-xs-1">
-                <i className="material-icons">info_outlined</i>
+                <i onClick={deleteItem} className="material-icons">close</i>
             </div>
         </div>
     )
