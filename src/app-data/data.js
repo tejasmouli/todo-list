f25cb997b65e74c9a44498777ba5c4883f6fd17f
tejasmouli <44651691+tejasmouli@users.noreply.github.com>
2019-12-19 01:42:35 +0530

    Minor changes to effect on deloyment

--- src/app-data/data.js
+++ src/app-data/data.js
@@ -3,7 +3,7 @@ const loadData = () => {
         {key:1, done: true, note: "Hi Hello", editState: false},
         {key:2, done: false, note: "No i cant do it now", editState: false},
         {key:3, done: false, note: "The World is so big that you will need a lot of money to complete one round around it. The World is so big that you will need a lot of money to complete one round around it.", editState: false},
-        {key:4, done: false, note: "No i cant do it now", editState: false}
+        {key:4, done: false, note: "why dont i do it now", editState: false}
     ];
 
     localStorage.setItem("allTodos", JSON.stringify(data));
